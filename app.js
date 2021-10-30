ef9523a443d6081af31ad710959b80ff485cd6df
Jessica Bryant <jessica.bryant@live.com>
2021-10-30 18:16:09 -0400

    Add Bonus 5b: addEventListener w/ ifElseIfCondition

--- app.js
+++ app.js
@@ -38,3 +38,23 @@ darkMode.addEventListener(`click`, () =>{ //listening for click to #dm button
 
 // 5a
 const reality = document.querySelector(`#reality`);
+
+// 5b
+let clickCount = 0; //this sets the variable clickCount to 0.
+reality.addEventListener(`click`, () => {  //this listens for the clicks
+    clickCount++;                           //and on each click, adds 1 to clickCount
+    if (clickCount < 3) {   //for the 1st and 2nd click, clickCount is under 3
+        alert(`You have successfully moved to another reality`); //this alert occurs
+    } else if (clickCount == 3)         //on the 3rd click, clickCount is 3
+        alert(`OH NO! You can only move to a new another reality a couple times. You are stuck in this reality!`); //this alert occurs
+}); //then after the 3rd click, for 4th and on up clicks, nothing happens. We're stuck!!!!!
+
+
+
+// } else if (c===3) {
+//     alert(`OH NO! You can only move to a new another reality a couple times. You are stuck in this reality!`);
+// }
+
+// let c = 0;
+//     if(c=1, c<=2, c++) {
+// 5b. Using addEventListener, have an alert that says "You have successfully moved to another reality" pop up when you click on the button. However, have this setup so that when the button is clicked a third time have the following alert display instead of the "You have successfully moved to another reality" alert. New alert "OH NO! You can only move to a new another reality a couple times. You are stuck in this reality!". And finally, when the button is clicked a fourth, fifth, sixth, etc. time have it do nothing (AKA Disable the button).
